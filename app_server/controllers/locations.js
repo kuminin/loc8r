eaca7a5777d367807356f2c3094427e41f5ae821
kuminin <kumin.in@hotmail.com>
2016-08-18 19:21:01 -0700

    Added objects to the controllers in locations.js and used them in the jade templates for review, location-info, and locations-list

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -6,6 +6,7 @@ module.exports.homelist = function(req, res) {
             title: 'Loc8r',
             strapline: 'Find places to work with wifi near you!'
         },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
         locations:[{
             name: 'Starcups',
             address: '125 High Street, Reading, RG6 1PS',
@@ -32,10 +33,55 @@ module.exports.homelist = function(req, res) {
 
 // Get 'Location info' page
 module.exports.locationInfo = function(req, res) {
-    res.render('location-info', { title: 'Location info' });
+    res.render('location-info', {
+        title: 'Starcups',
+        pageHeader: {title: 'Starcups'},
+        sidebar: {
+            context: 'is on Loc8r beause it has accessible wifi and space to sit down with your labtop and get some work done.',
+            callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+        },
+        location: {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            coords: {lat: 51.455041, lng: -0.9690844},
+            openingTimes: [{
+                days: 'Monday - Friday',
+                opening: '7:00am',
+                closing: '7:00pm',
+                closed: false
+            },
+            {
+                days: 'Saturday',
+                opening: '8:00am',
+                closing: '5:00pm',
+                closed: false
+            },
+            {
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            },
+            {
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+            }]
+        }
+    });
 };
 
 // Get 'Add review' page
 module.exports.addReview = function(req, res) {
-    res.render('location-review-form', { title: 'Add review'})
-}
+    res.render('location-review-form', {
+        title: 'Review Starcups on Loc8r',
+        pageHeader: { title: 'Review Starcups' }
+    });
+};
