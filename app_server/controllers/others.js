03c62f0104227c7e6c0ab6ec21075b3e79aea467
kuminin <kumin.in@hotmail.com>
2016-08-18 19:21:29 -0700

    Added object to the others.js and used them in the jade tempate for generic-text

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,7 @@
 // GET home page
 module.exports.about = function(req, res) {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+        title: 'About',
+        content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus reprehenderit enim perspiciatis nihil dolorum, itaque vitae ex consectetur facilis, maxime laborum eius reiciendis tenetur laudantium, officiis, voluptates cum sed quasi?'
+    });
 };
