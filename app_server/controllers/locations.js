2b6ec04bcffbdbf21e0f9900b38eb9097c04ae06
kuminin <kumin.in@hotmail.com>
2016-08-25 20:31:45 -0700

    Added Controller logic for doAddReview

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -135,4 +135,24 @@ module.exports.addReview = function(req, res) {
 };
 
 module.exports.doAddReview = function(req, res) {
+    var requestOptions, path, locationid, postdata;
+    locationid = req.params.locationid;
+    path = "/api/locations/" + locationid + '/reviews';
+    postdata = {
+        author: req.body.name,
+        rating: req.body.rating,
+        reviewText: req.body.review
+    };
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "POST",
+        json: postdata
+    };
+    request(requestOptions, function(err, response, body) {
+        if (response.statusCode === 201) {
+            res.redirect('/location/' + locationid);
+        } else {
+            _showError(req, res, response.statusCode);
+        }
+    });
 };
