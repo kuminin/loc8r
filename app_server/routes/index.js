6cb833f55e8713d586aed84ded49654c0bbfe79b
kuminin <kumin.in@hotmail.com>
2016-08-25 20:43:04 -0700

    Fixed doAddReview Routing

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -7,7 +7,7 @@ var ctrlOthers = require('../controllers/others');
 router.get('/', ctrlLocations.homelist);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
 router.get('/location/:locationid/reviews/new', ctrlLocations.addReview);
-router.post('/location/:locationid/reviews/new', ctrlLocations,doAddReview);
+router.post('/location/:locationid/reviews/new', ctrlLocations.doAddReview);
 
 // Other pages
 router.get('/about', ctrlOthers.about);
